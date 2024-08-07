05fd373e6e03b1bc4d0008793ba0e8a1fc74ea8f
Abhaya Achraya <aby.acharya.aws1gmail.com>
2024-08-07 22:42:32 +0100

    data_ingestion file updated

--- src/logger.py
+++ src/logger.py
@@ -1,3 +1,5 @@
+# logger.py
+
 import logging
 import os
 from datetime import datetime
@@ -21,3 +23,8 @@ logging.basicConfig(
 # Logger instance
 logger = logging.getLogger(__name__)
 
+if __name__ == "__main__":
+    logger.info("Starting script...")
+    logger.info("This is an info message.")
+    logger.warning("This is a warning message.")
+    logger.error("This is an error message.")
